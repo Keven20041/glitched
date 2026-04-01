7e84dff4f0dc881f568528974c843ab964c609b1
Keven20041 <116309709+Keven20041@users.noreply.github.com>
2026-04-01 17:48:22 -0400

    updated

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,4 +1,12 @@
 import { toNextJsHandler } from "better-auth/next-js";
 import { auth } from "@/app/lib/auth";
 
-export const { GET, POST } = toNextJsHandler(auth());
+export const GET = async (...args: Parameters<ReturnType<typeof toNextJsHandler>["GET"]>) => {
+	const { GET: getHandler } = toNextJsHandler(auth());
+	return getHandler(...args);
+};
+
+export const POST = async (...args: Parameters<ReturnType<typeof toNextJsHandler>["POST"]>) => {
+	const { POST: postHandler } = toNextJsHandler(auth());
+	return postHandler(...args);
+};
